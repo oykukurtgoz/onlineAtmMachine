aa2d21d30aac3acbb57473b308febc8ac8eb01d7
Oyku <oykukurtgoz@gmail.com>
2024-03-28 13:56:30 +0300

    user entrance is changed to a static method

--- src/Main.java
+++ src/Main.java
@@ -7,48 +7,19 @@ public class Main {
 
     static Customer recievedCustomer = new Customer();
 
+    static ArrayList<Customer> arrayList = new ArrayList<>();
     public static void main(String[] args) {
-        ArrayList<Customer> arrayList = new ArrayList<>();
+
         Account oykuAccount = new Account();
         CreditCard creditCard = new CreditCard(1500, oykuAccount);
         Customer customer = new Customer(1,"Oyku","Kurtgoz",12345,oykuAccount);
         System.out.println("Oyku account number: " + oykuAccount.getAccountNumber());
+
         arrayList.add(customer);
-        System.out.println("press 1 for sign up, press 2 for sign in");
+
+        enter();
+
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        if (a == 1) {
-            System.out.println("Enter your ID,name,last name and password ");
-            Account account = new Account();
-            CreditCard creditCard1 = new CreditCard(1500,account);
-            int id = scan.nextInt();
-            scan.nextLine();
-            String name = scan.nextLine();
-            String lastName = scan.nextLine();
-            int password = scan.nextInt();
-            Customer customer1 = new Customer(id, name, lastName, password,account,creditCard1);
-            arrayList.add(customer1);
-            logedInCustomer = customer1;
-            System.out.println("singed in successfully");
-        } else {
-            System.out.println("Enter your ID and password");
-            Customer customer2 = new Customer();
-            int id = scan.nextInt();
-            int password = scan.nextInt();
-            customer2.setPassword(password);
-            customer2.setId(id);
-            for (int i = 0; i < arrayList.size(); i++) {
-                if (customer2.id == arrayList.get(i).id) {
-                    if (customer2.password == arrayList.get(i).password) {
-                        System.out.println("entered successfully");
-                        logedInCustomer = arrayList.get(i);
-                        break;
-                    }else {
-                        System.out.println("wrong id or password");
-                    }
-                }
-            }
-        }
         while (true) {
             System.out.println("enter 1 to deposit\nenter 2 to withdraw \nenter 3 to transfer \n" +
                     "enter 4 to see your account");
@@ -96,14 +67,51 @@ public class Main {
                     break;
                 case 4:
                     System.out.println("Your asset: " + logedInCustomer.account.asset +
-                            "your debt: " + logedInCustomer.account.debt);
+                            " your debt: " + logedInCustomer.account.debt);
                     break;
             }
             System.out.println("to exit press 1, go back to menu press 2");
             int c = scan.nextInt();
             if (c == 1){
-                exit(1);
+                enter();
             }else continue;
+        }
+    }
+    public static void enter(){
+        System.out.println("press 1 for sign up, press 2 for sign in");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        if (a == 1) {
+            System.out.println("Enter your ID,name,last name and password ");
+            Account account = new Account();
+            CreditCard creditCard1 = new CreditCard(1500,account);
+            int id = scan.nextInt();
+            scan.nextLine();
+            String name = scan.nextLine();
+            String lastName = scan.nextLine();
+            int password = scan.nextInt();
+            Customer customer1 = new Customer(id, name, lastName, password,account,creditCard1);
+            arrayList.add(customer1);
+            logedInCustomer = customer1;
+            System.out.println("singed in successfully");
+        } else {
+            System.out.println("Enter your ID and password");
+            Customer customer2 = new Customer();
+            int id = scan.nextInt();
+            int password = scan.nextInt();
+            customer2.setPassword(password);
+            customer2.setId(id);
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (customer2.id == arrayList.get(i).id) {
+                    if (customer2.password == arrayList.get(i).password) {
+                        System.out.println("entered successfully");
+                        logedInCustomer = arrayList.get(i);
+                        break;
+                    }else {
+                        System.out.println("wrong id or password");
+                    }
+                }
+            }
         }
     }
 }
